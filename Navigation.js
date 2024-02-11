e4561e8789292b1980b9cfbb869c507b5d5265eb
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2024-02-11 17:27:47 -0600

    Tab navigator principal was created. Modified the icons with fontawesome5

---
+++ Navigation.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"; 
+import { NavigationContainer } from "@react-navigation/native"; 
+import Icon from 'react-native-vector-icons/FontAwesome5';
+//screens
+import HomeScreen from './screens/HomeScreen';
+import FinanceScreen from './screens/FinanceScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs(){
+    return(
+        <Tab.Navigator>
+            <Tab.Screen name="Detector" component={HomeScreen} options={{
+            tabBarIcon: ({color, size}) => <Icon name="camera" color={color} size={size}/>,
+            }}/>
+            <Tab.Screen name="Finanzas" component={FinanceScreen} options={{
+                tabBarIcon: ({color, size}) => <Icon name="money-check-alt" color={color} size={size}/>,
+            }}/>
+            <Tab.Screen name="Configuración" component={SettingsScreen} options={{
+                tabBarIcon: ({color, size}) => <Icon name="user-cog" color={color} size={size}/>,
+            }}/>
+        </Tab.Navigator>
+    );
+}
+
+export default function Navigation(){
+    return(
+        <NavigationContainer>
+            <MyTabs/>
+        </NavigationContainer>
+    );
+}
---
+++ screens/FinanceScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function FinanceScreen() {
+  return (
+    <View>
+      <Text>FinanceScreen</Text>
+    </View>
+  )
+}
