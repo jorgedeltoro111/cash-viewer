a9374da1e29367db40a469ddc48ef3c5ee9bdd04
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2024-03-14 19:20:45 -0600

    forms created successfully

--- components/IngresarDinero.js
+++ components/IngresarDinero.js
@@ -23,16 +23,16 @@ export default function IngresarDinero() {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.texto}>Monto</Text>
       <TextInput
-        style={styles.inputMonto}
+        style={styles.input}
         placeholder='230'
         onChangeText={handleInputMonto}
       />
       <Text style={styles.texto}>Concepto</Text>
       <TextInput
-        style={styles.inputConcepto}
+        style={styles.input}
         placeholder='Cine'
         onChangeText={handleInputText}
       />
@@ -42,11 +42,41 @@ export default function IngresarDinero() {
         onPress={() => {agregarDinero(monto, concepto)}}
         style={styles.botonAgregar}
       >
-        <Text style={styles.texto}>Agregar dinero</Text>
+        <Text style={styles.textoBoton}>Agregar dinero</Text>
       </TouchableOpacity>
     </View>
   )
 }
 const styles = StyleSheet.create({
-
+  container:{
+    flex: 1,
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#dee7e7'
+  },
+  texto:{
+    fontSize: 25,
+    marginTop: 15
+  },
+  input:{
+    borderRadius: 5,
+    borderWidth: 2,
+    padding: 10,
+    width: 200,
+    textAlign: 'center',
+    fontSize: 20,
+    backgroundColor: 'white'
+  },
+  botonAgregar:{
+    backgroundColor: '#61c26a',
+    borderWidth: 2, 
+    borderColor: 'black',
+    borderRadius: 15,
+    padding: 20,
+    margin: 10,
+    marginTop: 15,
+},
+textoBoton:{
+  fontSize: 20
+}
 })
